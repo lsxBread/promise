b85e57b76207cb20fc31bb0c1a123202feb52b21
Shixun Liu <shixun.liu1023@gmail.com>
2020-02-08 15:05:03 +1100

    setup test env

---
+++ test/index.ts
@@ -0,0 +1,13 @@
+import * as chai from 'chai';
+
+const assert = chai.assert;
+
+describe('Chai', () => {
+  it('it can test equal', () => {
+    assert(1 === 1);
+  });
+});
+
+describe('Promise is a Class', () => {
+  it('it has "then" function', () => {});
+});
